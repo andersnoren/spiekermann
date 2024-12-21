ef7eb691d076b28bd835c370d16ef22b4965d24b
Anders <anders@andersnoren.se>
2024-12-21 13:47:51 +0100

    Added hidden patterns where localized strings are used

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -5,20 +5,31 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained","contentSize":"21em"}} -->
-<div class="wp-block-group">
-	<!-- wp:heading {"textAlign":"center","level":1} -->
-	<h1 class="wp-block-heading has-text-align-center" id="page-not-found"><?php echo esc_html_x( 'Error 404', 'Heading for a webpage that is not found', 'spiekermann' ); ?></h1>
-	<!-- /wp:heading -->
 
-	<!-- wp:paragraph {"align":"center"} -->
-	<p class="has-text-align-center"><?php echo esc_html_x( 'We can’t find the page you’re looking for. Go back to the front page, or try the search form below.', 'Message to convey that a webpage could not be found', 'spiekermann' ); ?></p>
-	<!-- /wp:paragraph -->
-</div>
-<!-- /wp:group -->
+<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center">
+	<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"0"}}} -->
+	<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:heading {"level":1,"className":"ss00","fontSize":"massive"} -->
+		<h1 class="wp-block-heading ss00 has-massive-font-size"><?php echo esc_html_x( 'Page', '404: First word in "Page Not Found".', 'spiekermann' ); ?></h1>
+		<!-- /wp:heading -->
 
-<!-- wp:group {"layout":{"type":"constrained","contentSize":"240px"}} -->
-<div class="wp-block-group">
-	<!-- wp:pattern {"slug":"spiekermann/hidden-search"} /-->
+		<!-- wp:heading {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-3"}}}},"textColor":"contrast-3","fontSize":"massive"} -->
+		<h1 class="wp-block-heading has-contrast-3-color has-text-color has-link-color has-massive-font-size"><?php echo esc_html_x( 'Not', '404: Second word in "Page Not Found".', 'spiekermann' ); ?></h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:heading {"level":1,"className":"ss05","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-2"}}}},"textColor":"contrast-2","fontSize":"massive"} -->
+		<h1 class="wp-block-heading ss05 has-contrast-2-color has-text-color has-link-color has-massive-font-size"><?php echo esc_html_x( 'Found', '404: Third word in "Page Not Found".', 'spiekermann' ); ?></h1>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"center"} -->
+	<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:paragraph -->
+		<p class="has-text-align-center"><?php echo esc_html_x( 'Best return to the front page.', '404: Message to convey that a webpage could not be found', 'spiekermann' ); ?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
 </div>
-<!-- /wp:group -->
+<!-- /wp:columns -->
